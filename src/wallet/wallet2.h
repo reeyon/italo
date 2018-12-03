feb9338861f4a76f573d759d53d581d1c4507e2e
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2018-12-03 15:32:14 +0000

    wallet: optionally keep track of owned outputs uses

--- src/wallet/wallet2.h
+++ src/wallet/wallet2.h
@@ -1450,7 +1450,7 @@ namespace tools
   };
 }
 BOOST_CLASS_VERSION(tools::wallet2, 27)
-BOOST_CLASS_VERSION(tools::wallet2::transfer_details, 10)
+BOOST_CLASS_VERSION(tools::wallet2::transfer_details, 11)
 BOOST_CLASS_VERSION(tools::wallet2::multisig_info, 1)
 BOOST_CLASS_VERSION(tools::wallet2::multisig_info::LR, 0)
 BOOST_CLASS_VERSION(tools::wallet2::multisig_tx_set, 1)
