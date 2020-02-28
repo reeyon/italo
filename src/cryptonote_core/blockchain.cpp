d4670f88ae71d6453393f85c9efa0b7bc6e49fa6
reeyon <reeyonlim@gmail.com>
2020-02-28 15:18:07 +0800

    Update latest checkpoints

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -5155,7 +5155,7 @@ void Blockchain::cancel()
 }
 
 #if defined(PER_BLOCK_CHECKPOINT)
-static const char expected_block_hashes_hash[] = "205765142b163eb0f293c270e0cd6759ffb0165c3997259c7d148adf5a523675";
+static const char expected_block_hashes_hash[] = "1452a116d9ba2583729fe028d04bd87102d1f925a97eae46b4c3a11292cd89a3";
 void Blockchain::load_compiled_in_block_hashes(const GetCheckpointsCallback& get_checkpoints)
 {
   if (get_checkpoints == nullptr || !m_fast_sync)
